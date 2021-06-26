0d0dc14cb0c010cddd883ce1e5fd4c2f1f231dc0
Alejandro F <afuste7@gmail.com>
2021-06-26 18:49:45 -0400

    added server variable and used the applyMiddleware

--- server/server.js
+++ server/server.js
@@ -8,3 +8,10 @@ const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+server.applyMiddleware({ app });
