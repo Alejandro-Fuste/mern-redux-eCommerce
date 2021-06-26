c9dc3f71d4c3620fca4b02d706f0242a0ac0980e
Alejandro F <afuste7@gmail.com>
2021-06-26 18:53:19 -0400

    added middleware to serve up static images

--- server/server.js
+++ server/server.js
@@ -18,3 +18,6 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+app.use("/images", express.static(path.join(__dirname, "../client/images")));
