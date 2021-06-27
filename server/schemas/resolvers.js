524ec414bdc464da2737c48403182ef485ec06eb
Alejandro F <afuste7@gmail.com>
2021-06-27 15:39:22 -0400

    added resolvers for categores and products

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,6 +3,15 @@ const { User, Product, Category, Order } = require("../models");
 const { signToken } = require("../utils/auth");
 const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
 
-const resolvers = {};
+const resolvers = {
+  // Resolvers for any Queries
+
+  Query: {
+    categories: async () => {
+      return await Category.find();
+    },
+    products: async () => {},
+  },
+};
 
 module.exports = resolvers;
