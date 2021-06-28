d1976223b02f1076235c7cc0dac6041d904099f8
Alejandro F <afuste7@gmail.com>
2021-06-28 11:26:04 -0400

    added payload variable to signToken method

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -28,5 +28,7 @@ module.exports = {
 
     return req;
   },
-  signToken: function () {},
+  signToken: function ({ firstName, email, _id }) {
+    const payload = { firstName, email, _id };
+  },
 };
