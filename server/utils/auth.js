28958b8a9cbc10396ba4dca0115699bb6eec2a34
Alejandro F <afuste7@gmail.com>
2021-06-28 11:20:42 -0400

    added condition check for req.headers.authorization

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -7,6 +7,13 @@ module.exports = {
   authMiddleware: function ({ req }) {
     //   allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
+
+    // ['Bearer', '<tokenvalue>']
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim();
+    }
+
+    console.log("token", token);
   },
   signToken: function () {},
 };
