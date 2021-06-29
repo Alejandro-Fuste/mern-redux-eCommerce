07a3923f653f11d577810a8669af7bef71c0bae9
Alejandro F <afuste7@gmail.com>
2021-06-29 17:59:13 -0400

    added properties to user schema

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,29 @@ const { Schema } = mongoose;
 const bcrypt = require("bcrypt");
 const Order = require("./Order");
 
-const userSchema = new Schema({});
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  orders: [Order.schema],
+});
 
 const User = mongoose.model("User", userSchema);
 
