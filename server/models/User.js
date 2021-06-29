4901c78db5bf21164125aaabec10de67124be544
Alejandro F <afuste7@gmail.com>
2021-06-29 15:11:24 -0400

    added userSchema and User variables and exported User

--- server/models/User.js
+++ server/models/User.js
@@ -3,3 +3,9 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const bcrypt = require("bcrypt");
 const Order = require("./Order");
+
+const userSchema = new Schema({});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
