1ea86afe0928c34d3c27f0dd945cd90d1cfffbd6
Alejandro F <afuste7@gmail.com>
2021-06-29 14:54:30 -0400

    exported schemas

--- server/models/index.js
+++ server/models/index.js
@@ -2,3 +2,5 @@ const User = require("./User");
 const Product = require("./Product");
 const Category = require("./Catergory");
 const Order = require("./Order");
+
+module.exports = { User, Product, Category, Order };
