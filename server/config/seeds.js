c6ec1c0f659b1eb9690a4addae367d043997d8c6
Alejandro F <afuste7@gmail.com>
2021-06-29 11:56:37 -0400

    added comments to seed file

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -2,6 +2,7 @@ const db = require("./connection");
 const { User, Product, Category } = require("../models");
 
 db.once("open", async () => {
+  // Drop previously inserted categories so that new ones can be inserted
   await Category.deleteMany();
 
   const categories = await Category.insertMany([
@@ -13,4 +14,8 @@ db.once("open", async () => {
   ]);
 
   console.log("categories seeded");
+
+  // Drop previously inserted products so that new ones can be inserted
+
+  await Product.deleteMany();
 });
