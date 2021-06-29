e2a56b83df701a0e369d26b6b88e3022c791b00f
Alejandro F <afuste7@gmail.com>
2021-06-29 11:54:24 -0400

    added insertMany method to Categories to seed categories

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -3,4 +3,14 @@ const { User, Product, Category } = require("../models");
 
 db.once("open", async () => {
   await Category.deleteMany();
+
+  const categories = await Category.insertMany([
+    { name: "Food" },
+    { name: "Household Supplies" },
+    { name: "Electronics" },
+    { name: "Books" },
+    { name: "Toys" },
+  ]);
+
+  console.log("categories seeded");
 });
