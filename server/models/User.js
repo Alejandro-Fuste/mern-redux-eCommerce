b6065bf78856223ee320ba18f33389c60270a60b
Alejandro F <afuste7@gmail.com>
2021-06-29 18:10:07 -0400

    added hook to compare incoming password to hashed password

--- server/models/User.js
+++ server/models/User.js
@@ -36,6 +36,11 @@ userSchema.pre("save", async function (next) {
   }
 });
 
+// This hook compares the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
