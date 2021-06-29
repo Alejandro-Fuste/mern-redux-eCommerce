d695313d6a674dbf5068a949ea139108978e9fbf
Alejandro F <afuste7@gmail.com>
2021-06-29 11:52:29 -0400

    added deleteMany method to Category schema

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,4 +1,6 @@
 const db = require("./connection");
 const { User, Product, Category } = require("../models");
 
-db.once("open", async () => {});
+db.once("open", async () => {
+  await Category.deleteMany();
+});
