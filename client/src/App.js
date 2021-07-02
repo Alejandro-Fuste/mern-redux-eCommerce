651ffee2410e8c567cc28e5bf63a8ee9a0be03e2
Alejandro F <afuste7@gmail.com>
2021-07-02 15:40:55 -0400

    imported apollo/client package

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
 
 function App() {
   return (
