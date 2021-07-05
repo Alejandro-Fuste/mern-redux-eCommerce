3858ca4e6d9bc3d7c0b21df821d12553b94453fc
Alejandro F <afuste7@gmail.com>
2021-07-05 13:53:35 -0400

    added return statement with object in authLink variable

--- client/src/App.js
+++ client/src/App.js
@@ -26,6 +26,9 @@ const httpLink = createHttpLink({
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem("id_token");
+  return {
+    headers: { ...headers, authorization: token ? `Bearer ${token}` : "" },
+  };
 });
 
 function App() {
