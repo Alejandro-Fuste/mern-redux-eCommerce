c2e127c7574c7d2e1345a5ffb85e6865d8bd40d0
Alejandro F <afuste7@gmail.com>
2021-07-05 13:59:18 -0400

    added ApolloProvider component in app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -37,24 +37,7 @@ const client = new ApolloClient({
 });
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <ApolloProvider client={client}></ApolloProvider>;
 }
 
 export default App;
