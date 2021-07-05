55ecb375b956b4d6838126fe30455f78c5c1af1c
Alejandro F <afuste7@gmail.com>
2021-07-05 14:50:52 -0400

    added StripePromise into Cart component file

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -8,3 +8,5 @@ import Auth from "../../utils/auth";
 import { useDispatch, useSelector } from "react-redux";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import "./style.css";
+
+const stripePromise = loadStripe("");
