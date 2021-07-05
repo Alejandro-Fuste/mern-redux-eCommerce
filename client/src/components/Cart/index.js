35d00df20efd44d27735960503dda20fb7551221
Alejandro F <afuste7@gmail.com>
2021-07-05 14:40:17 -0400

    imported CartItem component into Cart component file

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -3,3 +3,4 @@ import { loadStripe } from "@stripe/stripe-js";
 import { useLazyQuery } from "@apollo/client";
 import { QUERY_CHECKOUT } from "../../utils/queries";
 import { idbPromise } from "../../utils/helpers";
+import CartItem from "../CartItem";
