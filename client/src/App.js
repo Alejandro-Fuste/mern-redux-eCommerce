6b6c3c3cabbf12d37e15d9fde25368ca76831d85
Alejandro F <afuste7@gmail.com>
2021-07-05 13:56:58 -0400

    added client variable to app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -31,6 +31,11 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
     <div className="App">
