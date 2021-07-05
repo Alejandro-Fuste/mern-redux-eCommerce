ee3d9580540b15cd9e262d14ab2eb64af29beb19
Alejandro F <afuste7@gmail.com>
2021-07-05 13:36:53 -0400

    imported Login page to app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import store from "./utils/store";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
+import Login from "./pages/Login";
 
 function App() {
   return (
