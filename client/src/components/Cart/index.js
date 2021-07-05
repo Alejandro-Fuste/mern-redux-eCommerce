7a45fcf4a911683b502e5f3a0461e8084ff15b05
Alejandro F <afuste7@gmail.com>
2021-07-05 14:41:12 -0400

    imported auth into Cart component file

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -4,3 +4,4 @@ import { useLazyQuery } from "@apollo/client";
 import { QUERY_CHECKOUT } from "../../utils/queries";
 import { idbPromise } from "../../utils/helpers";
 import CartItem from "../CartItem";
+import Auth from "../../utils/auth";
