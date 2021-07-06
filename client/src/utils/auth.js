b23075f9c73b5341220a3d045a33f541b0c8185b
Alejandro F <afuste7@gmail.com>
2021-07-06 18:37:44 -0400

    added code to body of getProfile method of AuthService class in auth.js file

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -5,7 +5,11 @@ class AuthService {
     return decode(this.getToken());
   }
 
-  loggedIn() {}
+  loggedIn() {
+    //   Checks if there is a saved token and it's still valid
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
+  }
 
   isTokenExpired(token) {}
 
