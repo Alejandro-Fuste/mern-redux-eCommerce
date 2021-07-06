1533fbd018ccd9db81fbcaaf06a03225baddffb8
Alejandro F <afuste7@gmail.com>
2021-07-06 19:21:48 -0400

    added request.onupgradeneeded method to body of idbPromise function in helpers.js file

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -8,5 +8,12 @@ export function pluralize(name, count) {
 export function idbPromise(storeName, method, object) {
   return new Promise((resolve, reject) => {
     const request = window.indexedDB.open("shop-shop", 1);
+    let db, tx, store;
+    request.onupgradeneeded = function () {
+      const db = request.result;
+      db.createObjectStore("products", { keyPath: "_id" });
+      db.createObjectStore("categories", { keyPath: "_id" });
+      db.createObjectStore("cart", { keyPath: "_id" });
+    };
   });
 }
