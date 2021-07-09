69b122bca9469eac700730bf4c7a3ab6222864a3
Alejandro F <afuste7@gmail.com>
2021-07-09 13:47:09 -0400

    exported five queries in queries.js file

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,11 @@
 import { gql } from "@apollo/client";
+
+export const QUERY_PRODUCTS = gql``;
+
+export const QUERY_CHECKOUT = gql``;
+
+export const QUERY_ALL_PRODUCTS = gql``;
+
+export const QUERY_CATEGORIES = gql``;
+
+export const QUERY_USER = gql``;
