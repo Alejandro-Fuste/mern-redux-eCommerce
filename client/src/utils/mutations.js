2b9c1b99578ed9227b8ac7048c3debf6d3bc2eb6
Alejandro F <afuste7@gmail.com>
2021-07-09 13:32:36 -0400

    added login mutations in mutations.js file

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,15 @@
 import { gql } from "@apollo/client";
 
-export const LOGIN = gql``;
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
 
 export const ADD_ORDER = gql``;
 
