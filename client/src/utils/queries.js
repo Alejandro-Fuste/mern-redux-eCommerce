a7cabbc3203a8ea6fa4417daf066fdf0b8216249
Alejandro F <afuste7@gmail.com>
2021-07-09 14:01:42 -0400

    added code to QUERY_USER in queries.js file

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -48,4 +48,23 @@ export const QUERY_CATEGORIES = gql`
   }
 `;
 
-export const QUERY_USER = gql``;
+export const QUERY_USER = gql`
+  {
+    user {
+      firstName
+      lastName
+      orders {
+        _id
+        purchaseDate
+        products {
+          _id
+          name
+          description
+          price
+          quantity
+          image
+        }
+      }
+    }
+  }
+`;
