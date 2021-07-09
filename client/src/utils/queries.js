e641d7ae93aa279038c2979eef2a22ec37d18bca
Alejandro F <afuste7@gmail.com>
2021-07-09 13:50:31 -0400

    added code to QUERY_PRODUCTS in queries.js file

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,6 +1,20 @@
 import { gql } from "@apollo/client";
 
-export const QUERY_PRODUCTS = gql``;
+export const QUERY_PRODUCTS = gql`
+  query getProducts($category: ID) {
+    products(category: $category) {
+      _id
+      name
+      description
+      price
+      quantity
+      image
+      category {
+        _id
+      }
+    }
+  }
+`;
 
 export const QUERY_CHECKOUT = gql``;
 
