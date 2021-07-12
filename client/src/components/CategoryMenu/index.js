c6a28e66995dbae123bf599eee88c0b5a14243b9
Alejandro F <afuste7@gmail.com>
2021-07-12 19:23:25 -0400

    imported actions file in CategoryMenu component

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,3 +1,7 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
 import { useDispatch, useSelector } from "react-redux";
+import {
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY,
+} from "../../utils/actions";
