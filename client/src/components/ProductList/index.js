43315cd75c7e2243ee490513d2a0620243c7d42a
Alejandro F <afuste7@gmail.com>
2021-07-13 17:38:08 -0400

    imported apollo client package in ProductList component

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,3 +1,4 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useQuery } from "@apollo/client";
 import ProductItem from "../ProductItem";
