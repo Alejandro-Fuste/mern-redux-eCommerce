dd9a61405f353f3002651ba555f4ccf877896c5f
Alejandro F <afuste7@gmail.com>
2021-07-13 16:44:27 -0400

    passed in item and destructured this properties in productItem function in ProductItem component

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -5,9 +5,11 @@ import { pluralize } from "../../utils/helpers";
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 
-function ProductItem() {
+function ProductItem(item) {
   const dispatch = useDispatch();
   const state = useSelector((state) => state);
+
+  const { image, name, id, price, quantity } = item;
 }
 
 export default ProductItem;
