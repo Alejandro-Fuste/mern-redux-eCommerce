9ff538efb9b1edbfc1a5c3ec4bf1153dd551041e
Alejandro F <afuste7@gmail.com>
2021-07-13 11:41:02 -0400

    added code to deleteBtn function in DeleteBtn component

--- client/src/components/DeleteBtn/index.js
+++ client/src/components/DeleteBtn/index.js
@@ -3,6 +3,12 @@ import React from "react";
 /* The ...props means, spread all of the passed props onto this element
  That way we don't have to define them all individually */
 
-function DeleteBtn(props) {}
+function DeleteBtn(props) {
+  return (
+    <span {...props} role="button" tabIndex="0">
+      X
+    </span>
+  );
+}
 
 export default DeleteBtn;
