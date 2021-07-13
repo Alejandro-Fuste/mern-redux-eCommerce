098dc503b946308f9aa982931a1357aab0d307ee
Alejandro F <afuste7@gmail.com>
2021-07-13 15:36:51 -0400

    added return statement into Nav component

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,7 +3,13 @@ import { Link } from "react-router-dom";
 import Auth from "../../utils/auth";
 
 function Nav() {
-  return <div></div>;
+  return (
+    <header>
+      <h1>
+        <Link></Link>
+      </h1>
+    </header>
+  );
 }
 
 export default Nav;
