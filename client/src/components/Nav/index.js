965703676dd527a670ed4357427b953c3a1fc7f1
Alejandro F <afuste7@gmail.com>
2021-07-13 15:54:54 -0400

    added login and signup links to else statement in showNavigation function in Nav component

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -21,7 +21,12 @@ function Nav() {
     } else {
       return (
         <ul className="flex-row">
-          <li className="mx-1"></li>
+          <li className="mx-1">
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li className="mx-1">
+            <Link to="/login">Login</Link>
+          </li>
         </ul>
       );
     }
