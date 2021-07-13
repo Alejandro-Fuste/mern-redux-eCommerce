87e1aa2ece5b37efed6d610ac30a047087df2a30
Alejandro F <afuste7@gmail.com>
2021-07-13 16:36:44 -0400

    added and exported productItem function in ProductItem component

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -2,3 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { pluralize } from "../../utils/helpers";
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
+
+function ProductItem() {}
+
+export default ProductItem;
