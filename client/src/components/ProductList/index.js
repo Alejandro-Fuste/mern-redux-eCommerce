ca43b3894c32e1ca899e593fe64e2f87c8f0ba7a
Alejandro F <afuste7@gmail.com>
2021-07-13 17:39:36 -0400

    imported actions file in ProductList component

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -2,3 +2,4 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useQuery } from "@apollo/client";
 import ProductItem from "../ProductItem";
+import { UPDATE_PRODUCTS } from "../../utils/actions";
