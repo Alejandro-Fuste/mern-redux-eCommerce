1229e60116bff90874ca8632a0ba404da9c700f0
Alejandro F <afuste7@gmail.com>
2021-07-14 19:33:59 -0400

    added ternary operator for rendering product in return statement in Detail page

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -84,7 +84,29 @@ function Detail() {
 
   return (
     <>
-      {}
+      {currentProduct && cart ? (
+        <div className="container my-1">
+          <Link to="/">← Back to Products</Link>
+
+          <h2>{currentProduct.name}</h2>
+
+          <p>
+            <strong>Price:</strong>${currentProduct.price}{" "}
+            <button onClick={addToCart}>Add to Cart</button>
+            <button
+              disabled={!cart.find((p) => p._id === currentProduct._id)}
+              onClick={removeFromCart}
+            >
+              Remove from Cart
+            </button>
+          </p>
+
+          <img
+            src={`/images/${currentProduct.image}`}
+            alt={currentProduct.name}
+          />
+        </div>
+      ) : null}
       {loading ? <img src={spinner} alt="loading" /> : null}
       <Cart />
     </>
