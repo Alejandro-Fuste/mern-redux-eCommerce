b75cb4fdac9666e8cd4505880e8141036e502f04
Alejandro F <afuste7@gmail.com>
2021-07-15 16:17:13 -0400

    imported auth file in Login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
 import { useMutation } from "@apollo/client";
 import { Link } from "react-router-dom";
-import { Login } from "../utils/mutations";
+import { LOGIN } from "../utils/mutations";
+import Auth from "../utils/auth";
