e7b1cf818271c09db13940713f5f0ef7bbb6436c
Alejandro F <afuste7@gmail.com>
2021-07-15 16:06:33 -0400

    removed ProductItem component import in Home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
-import ProductItem from "../components/ProductItem";
 
 const Home = () => {
   return (
