e2a6cf429dc1f097911d7918d4be9e2e383152d8
Alejandro F <afuste7@gmail.com>
2021-07-15 17:02:31 -0400

    added jsx code to return statement in Login function in Login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -5,31 +5,50 @@ import { LOGIN } from "../utils/mutations";
 import Auth from "../utils/auth";
 
 function Login(props) {
-  const [formState, setFormState] = useState({email: '', password: ''});
-  const [login, {error}] = useMutation(LOGIN);
+  const [formState, setFormState] = useState({ email: "", password: "" });
+  const [login, { error }] = useMutation(LOGIN);
 
   const handleFormSubmit = async (event) => {
-      event.preventDefault();
-      try {
-          const mutationResponse = await login({
-              variables: {email: formState.email, password: formState.password}
-          });
-          const token = mutationResponse.data.login.token;
-          Auth.login(token);
-      } catch (error) {
-          console.log(error);
-      }
+    event.preventDefault();
+    try {
+      const mutationResponse = await login({
+        variables: { email: formState.email, password: formState.password },
+      });
+      const token = mutationResponse.data.login.token;
+      Auth.login(token);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const handleChange = (event) => {
-      const {name, value} = event.target;
-      setFormState({
-          ...formState, 
-          [name]: value,
-        });
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
   };
 
-  return ()
+  return (
+    <div>
+      <h2></h2>
+
+      <form>
+        <div>
+          <label></label>
+          <input />
+        </div>
+        <div>
+          <label></label>
+          <input />
+        </div>
+        {}
+        <div>
+          <button></button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
