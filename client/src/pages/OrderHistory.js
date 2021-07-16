3a34368551166cc26be826592d8a687af498f558
Alejandro F <afuste7@gmail.com>
2021-07-16 14:25:02 -0400

    exported OrderHistory function in OrderHistory page

--- client/src/pages/OrderHistory.js
+++ client/src/pages/OrderHistory.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { QUERY_USER } from "../utils/queries";
 
-function OrderHistory() {}
+function OrderHistory() {
+  const { data } = useQuery(QUERY_USER);
+}
 
 export default OrderHistory;
