151e1acb7d5639e0857a52ca49dd2516fb43c311
Alejandro F <afuste7@gmail.com>
2021-07-16 15:56:48 -0400

    added code to jsx outline return statement in Signup page

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -31,29 +31,53 @@ function Signup(props) {
   };
 
   return (
-    <div>
-      <Link></Link>
-      <h2></h2>
+    <div className="container my-1">
+      <Link to="/login">← Go to Login</Link>
 
-      <form>
-        <div>
-          <label></label>
-          <input />
+      <h2>Signup</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            placeholder="First Name"
+            name="firstName"
+            type="firstName"
+            id="firstName"
+            onChange={handleChange}
+          />
         </div>
-        <div>
-          <label></label>
-          <input />
+        <div className="flex-row space-between my-2">
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            placeholder="lastName"
+            name="lastName"
+            type="lastName"
+            id="lastName"
+            onChange={handleChange}
+          />
         </div>
-        <div>
-          <label></label>
-          <input />
+        <div className="flex-row space-between my-2">
+          <label htmlFor="email">Email:</label>
+          <input
+            placeholder="youremail@test.com"
+            name="email"
+            type="email"
+            id="email"
+            onChange={handleChange}
+          />
         </div>
-        <div>
-          <label></label>
-          <input />
+        <div className="flex-row space-between my-2">
+          <label htmlFor="pwd">Password:</label>
+          <input
+            placeholder="*******"
+            name="password"
+            type="password"
+            id="pwd"
+            onChange={handleChange}
+          />
         </div>
-        <div>
-          <button></button>
+        <div className="flew-row flex-end">
+          <button type="submit">Submit</button>
         </div>
       </form>
     </div>
