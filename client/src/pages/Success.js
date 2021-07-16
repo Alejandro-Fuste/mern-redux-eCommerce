c82c308fea9f8c917567921ced8601556c5ce1fd
Alejandro F <afuste7@gmail.com>
2021-07-16 16:32:56 -0400

    added code to return statement in Success page

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -29,7 +29,15 @@ function Success() {
     saveOrder();
   }, [addOrder]);
 
-  return;
+  return (
+    <div>
+      <Jumbotron>
+        <h1>Success!</h1>
+        <h2>Thank you for your purchase!</h2>
+        <h2>You will now be redirected to the homepage</h2>
+      </Jumbotron>
+    </div>
+  );
 }
 
 export default Success;
