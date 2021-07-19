68db744cd5b3cc4c5b459fd2902f1bc5e686ccef
Alejandro F <afuste7@gmail.com>
2021-07-19 16:12:48 -0400

    removed usused variable

--- client/src/pages/OrderHistory.js
+++ client/src/pages/OrderHistory.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 
 import { useQuery } from "@apollo/client";
 import { QUERY_USER } from "../utils/queries";
-import { parse } from "graphql";
 
 function OrderHistory() {
   const { data } = useQuery(QUERY_USER);
