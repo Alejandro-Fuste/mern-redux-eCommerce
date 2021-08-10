24d52b2d6b30a12c7c6a935212a1c0a28c23fc3f
Alejandro F <afuste7@gmail.com>
2021-08-10 15:13:44 -0400

    added row and col components

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,6 +4,8 @@ import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
 
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Home = () => {
   return (
