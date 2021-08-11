adf5e5f9ff056ea82b933dc33e269c36320e4b4d
Alejandro F <afuste7@gmail.com>
2021-08-11 13:44:07 -0400

    added styling to button

--- client/src/components/CartModal/index.js
+++ client/src/components/CartModal/index.js
@@ -21,7 +21,7 @@ function CartModal() {
       >
         <Modal.Header closeButton>
           <Modal.Title id="example-custom-modal-styling-title">
-            Custom Modal Styling
+            Shopping Cart
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
