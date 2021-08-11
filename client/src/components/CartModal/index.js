287adc099503ef68f6114dcd27459b1e806dda1e
Alejandro F <afuste7@gmail.com>
2021-08-11 13:48:45 -0400

    added import statements from Cart component

--- client/src/components/CartModal/index.js
+++ client/src/components/CartModal/index.js
@@ -1,4 +1,13 @@
 import React, { useState, useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { useLazyQuery } from "@apollo/client";
+import { QUERY_CHECKOUT } from "../../utils/queries";
+import helpers from "../../utils/helpers";
+import CartItem from "../CartItem";
+import Auth from "../../utils/auth";
+import { useDispatch, useSelector } from "react-redux";
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
+
 import "./style.css";
 
 import Button from "react-bootstrap/Button";
