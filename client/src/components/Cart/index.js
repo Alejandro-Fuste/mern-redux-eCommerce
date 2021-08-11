3ea9b37ab97071e91576642c2af6af0d71560123
Alejandro F <afuste7@gmail.com>
2021-08-11 13:46:49 -0400

    removed commented out code

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { useLazyQuery } from "@apollo/client";
 import { QUERY_CHECKOUT } from "../../utils/queries";
-// import { idbPromise } from "../../utils/helpers";
 import helpers from "../../utils/helpers";
 import CartItem from "../CartItem";
 import Auth from "../../utils/auth";
