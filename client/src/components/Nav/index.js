4c1024d3078ea8f140262d088ff94a731aa96f11
Alejandro F <afuste7@gmail.com>
2021-08-12 17:11:47 -0400

    added style attritube

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -32,7 +32,7 @@ function Navi() {
   }
 
   return (
-    <Nav className="navParent">
+    <Nav style={{ display: "block" }}>
       <Navbar expand="lg" variant="dark" bg="dark">
         <Navbar.Brand href="/">
           <span role="img" aria-label="shopping bag">
