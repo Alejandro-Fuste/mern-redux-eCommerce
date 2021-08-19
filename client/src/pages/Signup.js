e7fa02a35ee14a534966e6a03f0cf3fa589cd141
Alejandro F <afuste7@gmail.com>
2021-08-19 17:52:29 -0400

    removed header tag

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -41,9 +41,6 @@ function Signup(props) {
       <Row id="signUpRow">
         <Col sm={10} id="signUpCol">
           <Form>
-            <header className="headerBackGround">
-              <span>A-Shop</span>
-            </header>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
               <Form.Control type="email" placeholder="Enter email" />
