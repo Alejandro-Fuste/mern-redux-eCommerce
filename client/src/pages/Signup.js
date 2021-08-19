b87d9d7b400a7e2f60322b2f32b96f9a6b4c32eb
Alejandro F <afuste7@gmail.com>
2021-08-19 19:36:29 -0400

    removed commented out code

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -93,57 +93,6 @@ function Signup(props) {
         </Col>
       </Row>
     </Container>
-
-    // <div className="container my-1">
-    //   <Link to="/login">← Go to Login</Link>
-
-    //   <h2>Signup</h2>
-    //   <form onSubmit={handleFormSubmit}>
-    //     <div className="flex-row space-between my-2">
-    //       <label htmlFor="firstName">First Name:</label>
-    //       <input
-    //         placeholder="First Name"
-    //         name="firstName"
-    //         type="firstName"
-    //         id="firstName"
-    //         onChange={handleChange}
-    //       />
-    //     </div>
-    //     <div className="flex-row space-between my-2">
-    //       <label htmlFor="lastName">Last Name:</label>
-    //       <input
-    //         placeholder="lastName"
-    //         name="lastName"
-    //         type="lastName"
-    //         id="lastName"
-    //         onChange={handleChange}
-    //       />
-    //     </div>
-    //     <div className="flex-row space-between my-2">
-    //       <label htmlFor="email">Email:</label>
-    //       <input
-    //         placeholder="youremail@test.com"
-    //         name="email"
-    //         type="email"
-    //         id="email"
-    //         onChange={handleChange}
-    //       />
-    //     </div>
-    //     <div className="flex-row space-between my-2">
-    //       <label htmlFor="pwd">Password:</label>
-    //       <input
-    //         placeholder="*******"
-    //         name="password"
-    //         type="password"
-    //         id="pwd"
-    //         onChange={handleChange}
-    //       />
-    //     </div>
-    //     <div className="flew-row flex-end">
-    //       <button type="submit">Submit</button>
-    //     </div>
-    //   </form>
-    // </div>
   );
 }
 
