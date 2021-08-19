0d14ea2f5b669f7c137340e9efea82074e676798
Alejandro F <afuste7@gmail.com>
2021-08-19 17:25:37 -0400

    added code for form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -8,6 +8,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 function Login(props) {
   const [formState, setFormState] = useState({ email: "", password: "" });
@@ -39,7 +40,26 @@ function Login(props) {
     <Container id="loginContainer">
       <Row id="loginRow">
         <Col sm={10} id="loginCol">
-          <span>Test Column</span>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
         </Col>
       </Row>
     </Container>
