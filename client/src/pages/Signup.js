49ef3475bb1288f0a5266fed303bdfb38d03bf92
Alejandro F <afuste7@gmail.com>
2021-08-19 17:06:53 -0400

    imported bootstrap container, row, and col

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -4,6 +4,11 @@ import { useMutation } from "@apollo/client";
 import Auth from "../utils/auth";
 import { ADD_USER } from "../utils/mutations";
 
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+
 function Signup(props) {
   const [formState, setFormState] = useState({ email: "", password: "" });
   const [addUser] = useMutation(ADD_USER);
