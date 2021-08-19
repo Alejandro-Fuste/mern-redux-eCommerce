3260e0c3719c8965c579a056cec661888ba46c67
Alejandro F <afuste7@gmail.com>
2021-08-19 14:55:13 -0400

    added components for container, row, and col

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -36,66 +36,49 @@ function Login(props) {
   };
 
   return (
-    <div className="container my-1">
-      <Link to="/signup">← Go to Signup</Link>
+    <Container>
+      <Row>
+        <Col></Col>
+      </Row>
+    </Container>
 
-      <h2>Login</h2>
+    // <div className="container my-1">
+    //   <Link to="/signup">← Go to Signup</Link>
 
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email Address:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="*******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        {error ? (
-          <div>
-            <p className="error-text">The provided credentials are incorrect</p>
-          </div>
-        ) : null}
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
+    //   <h2>Login</h2>
+
+    //   <form onSubmit={handleFormSubmit}>
+    //     <div className="flex-row space-between my-2">
+    //       <label htmlFor="email">Email Address:</label>
+    //       <input
+    //         placeholder="youremail@test.com"
+    //         name="email"
+    //         type="email"
+    //         id="email"
+    //         onChange={handleChange}
+    //       />
+    //     </div>
+    //     <div className="flex-row space-between my-2">
+    //       <label htmlFor="pwd">Password:</label>
+    //       <input
+    //         placeholder="*******"
+    //         name="password"
+    //         type="password"
+    //         id="pwd"
+    //         onChange={handleChange}
+    //       />
+    //     </div>
+    //     {error ? (
+    //       <div>
+    //         <p className="error-text">The provided credentials are incorrect</p>
+    //       </div>
+    //     ) : null}
+    //     <div className="flex-row flex-end">
+    //       <button type="submit">Submit</button>
+    //     </div>
+    //   </form>
+    // </div>
   );
 }
 
 export default Login;
-
-{
-  /* <Form>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
-
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form> */
-}
