981b6c34a0655e4bad9d02744bb652646a502873
Alejandro F <afuste7@gmail.com>
2021-08-19 17:46:51 -0400

    changed button text

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -39,7 +39,29 @@ function Signup(props) {
     <Container id="signUpContainer">
       <Row id="signUpRow">
         <Col sm={10} id="signUpCol">
-          <span>Test Column</span>
+          <Form>
+            <header className="headerBackGround">
+              <span>A-Shop</span>
+            </header>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Sign Up
+            </Button>
+          </Form>
         </Col>
       </Row>
     </Container>
