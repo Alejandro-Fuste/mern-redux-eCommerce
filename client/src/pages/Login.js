d85c80b341bea381329593641a78319cb0015f62
Alejandro F <afuste7@gmail.com>
2021-08-19 18:03:39 -0400

    added h2 tag

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -41,6 +41,7 @@ function Login(props) {
       <Row id="loginRow">
         <Col sm={10} id="loginCol">
           <Form>
+            <h2>Login</h2>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
               <Form.Control type="email" placeholder="Enter email" />
