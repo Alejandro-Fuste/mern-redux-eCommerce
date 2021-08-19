d25f94a31ed24c95ea2caafc548990c75a9f415a
Alejandro F <afuste7@gmail.com>
2021-08-19 19:31:32 -0400

    added form components for first and last names

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -42,6 +42,29 @@ function Signup(props) {
         <Col sm={10} id="signUpCol">
           <Form onSubmit={handleFormSubmit}>
             <h2 className="loginH2">Sign Up</h2>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                placeholder="First Name"
+                name="firstName"
+                type="firstName"
+                id="firstName"
+                onChange={handleChange}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                placeholder="lastName"
+                name="lastName"
+                type="lastName"
+                id="lastName"
+                onChange={handleChange}
+              />
+            </Form.Group>
+
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
               <Form.Control
@@ -51,14 +74,17 @@ function Signup(props) {
                 id="email"
                 onChange={handleChange}
               />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                type="password"
+                placeholder="*******"
+                name="password"
+                id="pwd"
+                onChange={handleChange}
+              />
             </Form.Group>
 
             <Button variant="primary" type="submit" className="formButton">
