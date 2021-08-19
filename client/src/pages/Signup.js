3fdead5f299609b0440d556f6775ca2c403289ca
Alejandro F <afuste7@gmail.com>
2021-08-19 17:50:35 -0400

    imported bootstrap button

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -8,6 +8,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 function Signup(props) {
   const [formState, setFormState] = useState({ email: "", password: "" });
