c98c704f35cd9133b51263b0d6493737a05165dc
Alejandro F <afuste7@gmail.com>
2021-08-19 18:15:43 -0400

    added attritubes to form input

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -40,11 +40,17 @@ function Login(props) {
     <Container id="loginContainer">
       <Row id="loginRow">
         <Col sm={10} id="loginCol">
-          <Form>
+          <Form onSubmit={handleFormSubmit}>
             <h2>Login</h2>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control
+                type="email"
+                placeholder="youremail@test.com"
+                name="email"
+                id="email"
+                onChange={handleChange}
+              />
               <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
               </Form.Text>
@@ -52,11 +58,15 @@ function Login(props) {
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+                id="pwd"
+                onChange={handleChange}
+              />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
+
             <Button variant="primary" type="submit">
               Log In
             </Button>
