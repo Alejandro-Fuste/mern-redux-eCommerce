d9adf1878476d2ddc52d6f8ab50b186ced43b03f
Alejandro F <afuste7@gmail.com>
2021-08-19 19:21:44 -0400

    added props to email

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -44,7 +44,13 @@ function Signup(props) {
             <h2 className="loginH2">Sign Up</h2>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control
+                type="email"
+                placeholder="youremail@test.com"
+                name="email"
+                id="email"
+                onChange={handleChange}
+              />
               <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
               </Form.Text>
