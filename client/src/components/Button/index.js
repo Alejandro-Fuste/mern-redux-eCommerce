e1f1be76b640bab3383841ca2fcd0fe851616cad
Alejandro F <afuste7@gmail.com>
2021-08-21 20:44:23 -0400

    added id prop to custom button

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 
-function CustomButton({ name, type, nameForClass, onClick }) {
+function CustomButton({ name, type, nameForClass, id, onClick }) {
   return (
     <>
       <Button
         variant="primary"
         type={type}
         className={nameForClass}
+        id={id}
         onClick={onClick}
       >
         {name}
