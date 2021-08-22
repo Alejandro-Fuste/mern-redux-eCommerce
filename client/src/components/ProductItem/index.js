d45a2d5483078080a29d5796a59a6c7021e3cbe6
Alejandro F <afuste7@gmail.com>
2021-08-21 20:24:32 -0400

    removed commented out code

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-// import { pluralize } from "../../utils/helpers";
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
-// import { idbPromise } from "../../utils/helpers";
 import helpers from "../../utils/helpers";
 
 const { idbPromise, pluralize } = helpers;
