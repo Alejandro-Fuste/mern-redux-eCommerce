7fceb25fe204214a2d08d3ce7957eee5d48f7311
Alejandro F <afuste7@gmail.com>
2021-08-24 18:39:53 -0400

    removed commented out

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -11,7 +11,6 @@ import {
   UPDATE_PRODUCTS,
 } from "../utils/actions";
 import { QUERY_PRODUCTS } from "../utils/queries";
-// import { idbPromise } from "../utils/helpers";
 import helpers from "../utils/helpers";
 import spinner from "../assets/spinner.gif";
 
