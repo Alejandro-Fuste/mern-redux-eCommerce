7e7334392b295b68db837b81db0f874ae061feaf
Alejandro F <afuste7@gmail.com>
2021-08-24 15:04:08 -0400

    changed margin value for top of productList

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -4,7 +4,6 @@ import { useQuery } from "@apollo/client";
 import ProductItem from "../ProductItem";
 import { UPDATE_PRODUCTS } from "../../utils/actions";
 import { QUERY_PRODUCTS } from "../../utils/queries";
-// import idbPromise from "../../utils/helpers";
 import helpers from "../../utils/helpers";
 import spinner from "../../assets/spinner.gif";
 
@@ -48,7 +47,7 @@ function ProductList() {
   }
 
   return (
-    <div className="my-2">
+    <div id="my-2">
       <h2>Our Products:</h2>
       {state.products.length ? (
         <div className="flex-row">
