5ab9c78bda2f45aa4d88936ebdcc098933b1e793
Alejandro F <afuste7@gmail.com>
2021-08-24 19:26:20 -0400

    added modal code

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -2,7 +2,27 @@ import React from "react";
 import Modal from "react-bootstrap/Modal";
 
 function modal() {
-  return <div></div>;
+  return (
+    <>
+      <Modal
+        show={show}
+        onHide={() => setShow(false)}
+        dialogClassName="modal-50w"
+        aria-labelledby="example-custom-modal-styling-title"
+        centered
+      >
+        <Modal.Header closeButton>
+          <img src={Logo} alt="logo" />
+          <Modal.Title id="example-custom-modal-styling-title">
+            A-Shop
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Oh snap! You added an item to your cart!</p>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
 }
 
 export default modal;
