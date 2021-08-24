dbb3c108a326d474bb2652c8e50b24c70051bd3d
Alejandro F <afuste7@gmail.com>
2021-08-24 13:19:45 -0400

    moved commented out code to productList component

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -109,13 +109,3 @@ function ProductItem(item) {
 }
 
 export default ProductItem;
-
-{
-  /* <Row xs={1} md={2} className="g-4">
-  {Array.from({ length: 4 }).map((_, idx) => (
-    <Col>
-      
-    </Col>
-  ))}
-</Row> */
-}
--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -72,3 +72,13 @@ function ProductList() {
 }
 
 export default ProductList;
+
+{
+  /* <Row xs={1} md={2} className="g-4">
+  {Array.from({ length: 4 }).map((_, idx) => (
+    <Col>
+      
+    </Col>
+  ))}
+</Row> */
+}
