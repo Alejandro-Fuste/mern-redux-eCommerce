7782cb7e2674383c2362193bae1c1a12d7272cc5
Alejandro F <afuste7@gmail.com>
2021-08-24 20:17:00 -0400

    imported col component

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -14,6 +14,7 @@ import spinner from "../assets/spinner.gif";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const { idbPromise } = helpers;
 
