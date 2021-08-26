c427464705981735f0369681241fa2d8f74cb904
Alejandro F <afuste7@gmail.com>
2021-08-26 15:16:43 -0400

    added code for modal

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -129,6 +129,23 @@ function Detail() {
             </div>
           ) : null}
           {loading ? <img src={spinner} alt="loading" /> : null}
+          <Modal
+            show={show}
+            onHide={() => setShow(false)}
+            dialogClassName="modal-50w"
+            aria-labelledby="example-custom-modal-styling-title"
+            centered
+          >
+            <Modal.Header closeButton>
+              <img src={Logo} alt="logo" />
+              <Modal.Title id="example-custom-modal-styling-title">
+                A-Shop
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <p>Oh snap! You added an item to your cart!</p>
+            </Modal.Body>
+          </Modal>
         </Col>
       </Row>
     </Container>
