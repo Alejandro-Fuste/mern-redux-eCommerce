a90012c07bb0ce51e94d0e6a8c1ab007d663d2e6
Alejandro F <afuste7@gmail.com>
2021-08-27 22:32:20 -0400

    imported button component

--- client/src/components/CartModal/index.js
+++ client/src/components/CartModal/index.js
@@ -7,7 +7,7 @@ import CartItem from "../CartItem";
 import Auth from "../../utils/auth";
 import { useDispatch, useSelector } from "react-redux";
 import { ADD_MULTIPLE_TO_CART } from "../../utils/actions";
-
+import Button from "../Button";
 import "./style.css";
 
 import Button from "react-bootstrap/Button";
