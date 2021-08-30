0e0af44b4dbbaa743cf451b96703a5dcb4fe157e
Alejandro F <afuste7@gmail.com>
2021-08-30 15:27:53 -0400

    added destructed props and code to return statement

--- client/src/components/Error/index.js
+++ client/src/components/Error/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-function Error() {
-  return <div></div>;
+function Error({ className, ariaLabel, text }) {
+  return (
+    <>
+      <h3 id="noItemsMessage">
+        <span role="img" aria-label="shocked" id="noItemsImage">
+          😱
+        </span>
+        You haven't added anything to your cart yet!
+      </h3>
+    </>
+  );
 }
 
 export default Error;
