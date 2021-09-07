b80d46c1fbfe8214597e6631a40031b1854d1b59
Alejandro F <afuste7@gmail.com>
2021-09-07 15:08:36 -0400

    imported home page and added shop import

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import { Provider } from "react-redux";
 import store from "./utils/store";
 
 import Home from "./pages/Home";
+import Shop from "./pages/Shop";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
