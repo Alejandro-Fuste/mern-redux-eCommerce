8ac4af4226474a9f931d8f31510c7cdc49e0ed37
Alejandro F <afuste7@gmail.com>
2021-09-07 15:09:46 -0400

    removed commented out code

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import { useMutation } from "@apollo/client";
 import Jumbotron from "../components/Jumbotron";
 import { ADD_ORDER } from "../utils/mutations";
-// import { idbPromise } from "../utils/helpers";
 import helpers from "../utils/helpers";
 
 const { idbPromise } = helpers;
