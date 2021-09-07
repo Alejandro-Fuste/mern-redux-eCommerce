a31183c6d76dffb7207b1ff02970a8cd67dc5d19
Alejandro F <afuste7@gmail.com>
2021-09-07 15:19:54 -0400

    added container, row, and col components

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 const Home = () => {
-    return ()
-}
+  return (
+    <Container>
+      <Row>
+        <Col></Col>
+      </Row>
+    </Container>
+  );
+};
 
-export default Home
+export default Home;
