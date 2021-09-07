b4229342169bb35251b5f4e44de579413a62b1eb
Alejandro F <afuste7@gmail.com>
2021-09-07 15:20:26 -0400

    removed commented out code

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
-// import Cart from "../components/Cart";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
