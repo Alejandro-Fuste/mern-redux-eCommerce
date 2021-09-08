5e7d73256ea021027f5e419c88be395502675ad3
Alejandro F <afuste7@gmail.com>
2021-09-08 16:46:11 -0400

    moved code from WaveBorder to Image component

--- client/src/components/Image/index.js
+++ client/src/components/Image/index.js
@@ -1,7 +1,14 @@
 import React from "react";
+import BlueGif from "../../assets/CreditCardBlue.svg";
 
 const Image = () => {
-  return;
+  return (
+    <>
+      <span>
+        <img src={BlueGif} alt="Credit Cart Payment" />
+      </span>
+    </>
+  );
 };
 
 export default Image;
--- client/src/components/WaveBorder/index.js
+++ client/src/components/WaveBorder/index.js
@@ -2,13 +2,7 @@ import React from "react";
 import BlueGif from "../../assets/CreditCardBlue.svg";
 
 const WaveBorder = () => {
-  return (
-    <>
-      <span>
-        <img src={BlueGif} alt="Credit Cart Payment" />
-      </span>
-    </>
-  );
+  return <></>;
 };
 
 export default WaveBorder;
