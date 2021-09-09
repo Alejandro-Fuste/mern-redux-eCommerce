fb56284eb9772c31dac4ba2a9605aa7ff900db8c
Alejandro F <afuste7@gmail.com>
2021-09-08 20:17:10 -0400

    added text box component to home page

--- client/src/components/TextBox/index.js
+++ client/src/components/TextBox/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-const TextBox = () => {
-  return <></>;
+const TextBox = ({ textHeading, pText }) => {
+  return (
+    <>
+      <h2>{textHeading}</h2>
+      <p>{pText}</p>
+    </>
+  );
 };
 
 export default TextBox;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "../components/Image";
+import TextBox from "../components/TextBox";
 
 import { ReactComponent as Wave } from "../assets/wave1.svg";
 import imageSrc from "../assets/CreditCardBlue.svg";
@@ -15,6 +16,10 @@ const Home = () => {
         <Col>
           <Wave />
           <Image src={imageSrc} alt="Credit Card" id="creditCard" />
+          <TextBox
+            textHeading="Text Heading"
+            pText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo nulla facilisi nullam vehicula. Elementum nibh tellus molestie nunc. Volutpat ac tincidunt vitae semper quis lectus nulla at volutpat. "
+          />
         </Col>
       </Row>
     </Container>
