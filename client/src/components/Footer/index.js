78c8ab81e47be31aed2a881649b61e437ba5e55f
Alejandro F <afuste7@gmail.com>
2021-09-09 18:59:54 -0400

    imported stylesheet and added styling

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faIdCard } from "@fortawesome/free-solid-svg-icons";
+import "./style.css";
 
 const Footer = () => {
   return (
