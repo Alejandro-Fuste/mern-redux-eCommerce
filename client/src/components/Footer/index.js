5c0a23b47790b1fbb1f76802860d339777b5bf6e
Alejandro F <afuste7@gmail.com>
2021-09-08 21:10:44 -0400

    install icons and added them to index file

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faIdCard } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
-  return <></>;
+  return (
+    <div>
+      <span>
+        <FontAwesomeIcon icon={faLinkedin} />
+      </span>
+      <span>
+        <FontAwesomeIcon icon={faGithub} />
+      </span>
+      <span>
+        <FontAwesomeIcon icon={faIdCard} />
+      </span>
+    </div>
+  );
 };
 
 export default Footer;
