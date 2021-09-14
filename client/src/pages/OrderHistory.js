6616b4c4c2ebd8d62ebb49e54e4c54aed99dfcf5
Alejandro F <afuste7@gmail.com>
2021-09-14 14:54:32 -0400

    imported bootstrap container, row, and col

--- client/src/pages/OrderHistory.js
+++ client/src/pages/OrderHistory.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
 import { useQuery } from "@apollo/client";
 import { QUERY_USER } from "../utils/queries";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function OrderHistory() {
   const { data } = useQuery(QUERY_USER);
