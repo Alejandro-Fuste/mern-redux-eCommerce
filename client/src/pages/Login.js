0826de96f0e29466919a681db8398a247697af66
Alejandro F <afuste7@gmail.com>
2021-09-14 22:51:46 -0400

    added Nav component to login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -9,6 +9,7 @@ import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 
 import Button from "../components/Button";
+import Nav from "../components/Nav";
 
 function Login(props) {
   const [formState, setFormState] = useState({ email: "", password: "" });
@@ -38,6 +39,7 @@ function Login(props) {
 
   return (
     <Container fluid style={{ padding: "0px" }} id="loginContainer">
+      <Nav />
       <Row id="loginRow">
         <Col sm={10} id="loginCol">
           <Form onSubmit={handleFormSubmit} className="loginForm">
