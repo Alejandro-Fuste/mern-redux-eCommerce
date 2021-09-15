e0244ad2884019fadf433f47b25f90e741f2c0e6
Alejandro F <afuste7@gmail.com>
2021-09-15 13:52:00 -0400

    removed commented out code

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -4,9 +4,6 @@ function Jumbotron({ children }) {
   return (
     <div
       style={{
-        // height: 560,
-        // clear: "both",
-        // paddingTop: 120,
         textAlign: "center",
         color: "#434e54",
       }}
