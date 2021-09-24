298a75ec26de19469932ea5babf7b6265db4fde5
Alejandro F <afuste7@gmail.com>
2021-09-24 18:56:39 -0400

    imported and added container, row, and col components

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,18 +1,28 @@
 import React from "react";
 import Jumbotron from "../components/Jumbotron";
 
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 const NoMatch = () => {
   return (
-    <div>
-      <Jumbotron>
-        <h1>404 Page Not Found</h1>
-        <h1>
-          <span role="img" aria-label="Face With Rolling Eyes Emoji">
-            🙄
-          </span>
-        </h1>
-      </Jumbotron>
-    </div>
+    <Container fluid id="noMatchContainer">
+      <Row>
+        <Col xs={12}></Col>
+      </Row>
+    </Container>
+
+    // <div>
+    //   <Jumbotron>
+    //     <h1>404 Page Not Found</h1>
+    //     <h1>
+    //       <span role="img" aria-label="Face With Rolling Eyes Emoji">
+    //         🙄
+    //       </span>
+    //     </h1>
+    //   </Jumbotron>
+    // </div>
   );
 };
 
