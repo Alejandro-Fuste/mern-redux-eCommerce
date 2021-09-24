8d7a77d2187c0e34ba48623ae23d09c6505237c0
Alejandro F <afuste7@gmail.com>
2021-09-24 19:26:08 -0400

    imported and added svg

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -3,6 +3,8 @@ import Image from "../components/Image";
 import TextBox from "../components/TextBox";
 import Jumbotron from "../components/Jumbotron";
 
+import imageSrc from "../assets/noMatch.svg";
+
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -14,7 +16,7 @@ const NoMatch = () => {
         <Col xs={12} id="noMatchCol">
           <div>
             <span id="backgroundCircle"></span>
-            {/* <Image src={imageSrc} alt="No Match" id="noMatchImage" /> */}
+            <Image src={imageSrc} alt="No Match" id="noMatchImage" />
           </div>
           <TextBox id="successTextBox">
             <p>Oops, something went wrong!</p>
