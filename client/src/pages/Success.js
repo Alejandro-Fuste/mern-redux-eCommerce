f6c5e682ec673af74c8d687d873eb351bbdac523
Alejandro F <afuste7@gmail.com>
2021-09-25 20:29:53 -0400

    commented out text from textBox

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -54,8 +54,8 @@ function Success() {
           <Image src={imageSrc} alt="Success" id="successImage" />
           <TextBox id="successTextBox">
             <h1>Success!</h1>
-            <h2>Thank you for your purchase!</h2>
-            <h2>You will now be redirected to the homepage</h2>
+            {/* <h2>Thank you for your purchase!</h2>
+            <h2>You will now be redirected to the homepage</h2> */}
           </TextBox>
         </Col>
       </Row>
