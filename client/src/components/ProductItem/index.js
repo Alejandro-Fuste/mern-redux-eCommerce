51f5892cd9b6877dd8c4c6c35d3a75fe91b41f0b
Alejandro F <afuste7@gmail.com>
2021-10-06 13:15:50 -0400

    commited out code related to add to cart function

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,47 +1,49 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
-import helpers from "../../utils/helpers";
+// import { useSelector } from "react-redux";
+// import { useDispatch, useSelector } from "react-redux";
+// import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+// import helpers from "../../utils/helpers";
 import Logo from "../../assets/logo.svg";
 import "./style.css";
 
 import Card from "react-bootstrap/Card";
 import Modal from "react-bootstrap/Modal";
 
-const { idbPromise } = helpers;
+// const { idbPromise } = helpers;
 
 function ProductItem(item) {
   const [show, setShow] = useState(false);
-  const dispatch = useDispatch();
-  const state = useSelector((state) => state);
+  // const dispatch = useDispatch();
+  // const state = useSelector((state) => state);
 
-  const { image, name, _id, price, quantity } = item;
+  // const { image, name, _id, price, quantity } = item;
+  const { image, name, _id, price } = item;
 
-  const { cart } = state;
+  // const { cart } = state;
 
-  const addToCart = () => {
-    const itemInCart = cart.find((cartItem) => cartItem._id === _id);
-    if (itemInCart) {
-      dispatch({
-        type: UPDATE_CART_QUANTITY,
-        _id: _id,
-        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
-      });
-      idbPromise("cart", "put", {
-        ...itemInCart,
-        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
-      });
-      setShow(true);
-    } else {
-      dispatch({
-        type: ADD_TO_CART,
-        product: { ...item, purchaseQuantity: 1 },
-      });
-      idbPromise("cart", "put", { ...item, purchaseQuantity: 1 });
-      setShow(true);
-    }
-  };
+  // const addToCart = () => {
+  //   const itemInCart = cart.find((cartItem) => cartItem._id === _id);
+  //   if (itemInCart) {
+  //     dispatch({
+  //       type: UPDATE_CART_QUANTITY,
+  //       _id: _id,
+  //       purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+  //     });
+  //     idbPromise("cart", "put", {
+  //       ...itemInCart,
+  //       purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+  //     });
+  //     setShow(true);
+  //   } else {
+  //     dispatch({
+  //       type: ADD_TO_CART,
+  //       product: { ...item, purchaseQuantity: 1 },
+  //     });
+  //     idbPromise("cart", "put", { ...item, purchaseQuantity: 1 });
+  //     setShow(true);
+  //   }
+  // };
 
   return (
     <>
